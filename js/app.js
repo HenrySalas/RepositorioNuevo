d295658019a466012bc40a9b52d4a6132099c788
Henry Salas <arturhenry89@gmail.com>
2016-02-19 12:12:07 -0600

    Mi primer commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('UserApp', ['ngRoute']);
+
+app.config(function ($routeProvider) { 
+  $routeProvider 
+    .when('/home', { 
+      controller: 'HomeController', 
+      templateUrl: 'views/home.html' 
+    })
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+});
